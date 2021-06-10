aeadcd029e4ec31f31d31c46038f67dbc595f577
BlancaMunizaga <bmunizaga1@uc.cl>
2021-06-10 15:03:35 -0400

    main

--- src/main/main.c
+++ src/main/main.c
@@ -4,84 +4,103 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-    // Primero por cada partición hacemos os_mount imprimimos  el mbt y vemos los acrhivos que tienen cada partición (os_ls)
-    // for(int i = 0; i<5; i++){
-    //     printf("\n\nProbando particion %i\n", i);
-    //     os_mount("src/discos/simdisk.bin", i);
-    //     if (i==0){
-    //         os_mbt();
-    //     }
-    //     if(particion_valida){
-    //         os_ls();
-    //     }
-        
-    //     fclose(disco);
-    // }
-    
-    //Montamos la segunda particion
-    os_mount("src/discos/simdisk.bin", 0);
-    // printf("Disco antes de la nueva particion: \n");
-    // //Imprimimos la mbt
-    // os_mbt();
-    // printf("Archivos: \n");
-    // //Imprimimos los archivos de la particion
-    // os_ls();
-    // printf("Bitmap: \n");
-    // //Imprimimos todos los bloques bitmap y la cantidad bloques libres y ocupados
-    // os_bitmap(0);
-    
-    //Creamos la particion
-    // printf("Disco después de la nueva particion: \n");
-    // os_create_partition(6, 4000);
-    // //Imprimimos la mbt
-    // os_mbt();
-    //Borramos la particion
-    //os_delete_partition(6);
-    //Volvemos a imprimir la mbt
-    //os_mbt();
-    
-    // //Mostramos los archivos existentes en la particion
-    // os_ls();
-    // // Vemos si en la partición existe el archivo mystery.mp3 (que exixte)
-    // printf("");
-    // int ret = os_exists("mystery.mp3");
-    // printf("exists = %d\n", ret);
-    // //Vemos si en la partición existe mysteri.mp3 (que no existe)
-    // int ret1 = os_exists("mysteri.mp3");
-    // printf("exists = %d\n", ret1);
-    // // eliminamos el archivo mystery.mp3
-    // os_rm("mystery.mp3");
-    // // Hacemos os_ls para mostrar que el archivo fue borrado
-    // os_ls();
+    if (argv[1] == "0") // monta el disco y muestra el masterboot table y luego muestra los archivos  de cada partición
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            printf("\n\nProbando particion %i\n", i);
+            os_mount("src/discos/simdisk.bin", i);
+            if (i == 0)
+            {
+                os_mbt();
+            }
+            if (particion_valida)
+            {
+                os_ls();
+            }
 
-    // //Cerramos esta particion
-    // fclose(disco);
-    
-    // //Montamos la siguiente particion, la 4
-    // os_mount("src/discos/simdisk.bin", 4);
-    // //Usamos os_open para abrir en modo de lectura nene.txt
-    // osFile * file_desc = os_open("nene.txt", 'r');
-    // unsigned char buffer[file_desc->tamano];
-    // // leemos el archivo completo
-    // int bytes_leidos = os_read(file_desc, buffer, file_desc->tamano);
-    // // y escribimos los bytes en un  archivo txt para ver el resultado
-    // FILE* prueba = fopen("pruebaaaaaa.txt", "wb+");
-    // for (int i = 0; i<file_desc->tamano; i++)
-    // {
-    //     fputc(buffer[i], prueba);
-    // }
-    // fclose(prueba);
-    // // Cerramos el arhivo nene.txt (ver leaks para comprobar)
-    // os_close(file_desc);
-    // printf("bytes leidos = %d\n", bytes_leidos);
-
-    // //Mostramos el estado actual de la mbt
-    // os_mbt();
-    // //Reseteamos la mbt
-    // os_reset_mbt();
-    // //Mostramos como quedo la mbt
-    // os_mbt();
-    fclose(disco);
+            fclose(disco);
+        }
+    }
+    else if (argv[1] == "1")
+    { // el disco que se quiere montar es invalido porque no existe el archivo
+        os_mount("src/discos/disco_no_existe.bin", 0);
+    }
+    else if (argv[1] == "2")
+    {
+        // Ver si un archivo existe.
+        char *filename = argv[2];
+        os_mount("src/discos/simdisk.bin", 4);
+        printf("Viendo si existe el archivo [%s]\n", filename);
+        int exists = os_exists(filename);
+        printf("Borrando el archivo [%s]\n", filename);
+        os_rm(filename);
+        printf("Intentando ver archivo si existe el archivo recien borrado.\n");
+        int exists = os_exists(filename);
+        printf("Intentando borrar archivo que no existe \n");
+        os_rm(filename);
+        fclose(disco);
+    }
+    else if (argv[1] == "3")
+    { //montamos el disco y mostramos todo el bitmap
+        os_mount("src/discos/simdisk.bin", 4);
+        printf("Imprimiendo todo el bitmap\n");
+        os_bitmap(0);
+        //mostramos un bloque especifico del bitmap
+        printf("Imprimiendo el bloque 2 del bitmap\n");
+        os_bitmap(2);
+        //Pedir un bloque invalido
+        printf("Imprimiendo bloque invalido (20) del bitmap\n");
+        os_bitmap(20);
+        fclose(disco);
+    }
+    else if (argv[1] == "4")
+    {
+        //Montamos la 4 partición 4
+        os_mount("src/discos/simdisk.bin", 4);
+        printf("Disco antes de la nueva particion: \n");
+        //Imprimimos la mbt
+        os_mbt();
+        //Creamos la particion
+        printf("Disco después de la nueva particion: \n");
+        os_create_partition(6, 4000);
+        //Imprimimos la mbt
+        os_mbt();
+        //Borramos la particion
+        printf("Disco después de borrar la particion recién creada: \n");
+        os_delete_partition(6);
+        //Volvemos a imprimir la mbt
+        printf("Mostrando mbt despues de borrar la particion: \n");
+        os_mbt();
+        //Probando error de borrar particion invalida
+        printf("Intentando borrar particion invalida: \n");
+        os_delete_partition(6);
+        // Probar errores
+        printf("Probando crear una partición demasiado grande: \n");
+        os_create_partition(6, 400000);
+        // Va a tirar error.
+        printf("Probando crear una partición con ID invalido: \n");
+        os_create_partition(180, 400);
+        fclose(disco);
+    }
+    else if (argv[1] == "5")
+    {
+        //Montamos la 4 partición 4
+        os_mount("src/discos/simdisk.bin", 4);
+        //Usamos os_open para abrir en modo de lectura nene.txt
+        osFile *file_desc = os_open("nene.txt", 'r');
+        //lo leemos entero
+        unsigned char buffer[file_desc->tamano];
+        int bytes_leidos = os_read(file_desc, buffer, file_desc->tamano);
+        // y escribimos los bytes en un  archivo txt para ver el resultado
+        for (int i = 0; i < file_desc->tamano; i++)
+        {
+            printf("%s", buffer[i]);
+        }
+        os_close(file_desc);
+        printf("bytes leidos = %d\n", bytes_leidos);
+        fclose(disco);
+    }
 }
